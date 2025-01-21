7e26e5291141b0dc06be97801b5b8ef05e5d12cf
안동환 <ddh1713flrwls1713@gmail.com>
2025-01-21 21:19:00 +0900

    first commit

---
+++ src/main/java/hello/javamid2/JavaMid2Application.java
@@ -0,0 +1,13 @@
+package hello.javamid2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMid2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMid2Application.class, args);
+    }
+
+}
---
+++ src/test/java/hello/javamid2/JavaMid2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.javamid2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMid2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
