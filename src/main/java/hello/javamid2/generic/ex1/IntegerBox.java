008b9e7d8cef62ea129ab37585ff25f75d605664
안동환 <ddh1713flrwls1713@gmail.com>
2025-01-23 22:20:07 +0900

    제네릭1

---
+++ src/main/java/hello/javamid2/generic/ex1/IntegerBox.java
@@ -0,0 +1,14 @@
+package hello.javamid2.generic.ex1;
+
+public class IntegerBox {
+
+    public Integer value;
+
+    public void set(Integer value) {
+        this.value = value;
+    }
+
+    public Integer get() {
+        return value;
+    }
+}
---
+++ src/main/java/hello/javamid2/generic/ex1/ObjectBox.java
@@ -0,0 +1,14 @@
+package hello.javamid2.generic.ex1;
+
+public class ObjectBox {
+
+    public Object value;
+
+    public void set(Object value) {
+        this.value = value;
+    }
+
+    public Object get() {
+        return value;
+    }
+}
